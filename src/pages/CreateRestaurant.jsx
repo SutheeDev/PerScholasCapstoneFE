51fcd574877235f3ef1f453dde22d5a95bbfd6d3
SU-T <guitaristpeet@gmail.com>
2025-01-30 10:34:52 -0500

    added form inputs to createRestaurant page

--- src/pages/CreateRestaurant.jsx
+++ src/pages/CreateRestaurant.jsx
@@ -1,16 +1,72 @@
 import styled from "styled-components";
 
+// Import Icons
+import { CiStar } from "react-icons/ci";
+import { TiStarFullOutline } from "react-icons/ti";
+import { BiDollar } from "react-icons/bi";
+
 const CreateRestaurant = () => {
   return (
-    <section>
-      <CardsContainer>
-        <h1 className="heading">Create Entry</h1>
-      </CardsContainer>
-    </section>
+    // <section>
+    <CardsContainer>
+      <h1 className="heading">Create Entry</h1>
+      <form action="">
+        <input className="image-upload" type="file" name="" id="" />
+        <div className="form-inputs">
+          <label htmlFor="title">Title</label>
+          <input type="text" name="title" id="title" />
+          <label htmlFor="review">Review</label>
+          <textarea name="review" id="review"></textarea>
+          <label htmlFor="cuisine">Cuisine</label>
+          <input type="text" name="cuisine" id="cuisine" />
+          <label htmlFor="date">Date Visit</label>
+          <input type="date" name="date" id="date" />
+          <label htmlFor="rating">Rating</label>
+          <TiStarFullOutline className="star" />
+          <TiStarFullOutline className="star" />
+          <TiStarFullOutline className="star" />
+          <TiStarFullOutline className="star" />
+          <TiStarFullOutline className="star" />
+          <label htmlFor="price">Price</label>
+          <BiDollar className="dollar" />
+          <BiDollar className="dollar" />
+          <BiDollar className="dollar" />
+          <BiDollar className="dollar" />
+          <BiDollar className="dollar" />
+          <button>Save Entry</button>
+          <button>Cancel</button>
+        </div>
+      </form>
+    </CardsContainer>
+    // {/* </section> */}
   );
 };
 export default CreateRestaurant;
 
 const CardsContainer = styled.div`
   padding-right: var(--container-padding);
+  width: 100%;
+  /* background-color: aliceblue; */
+
+  form {
+    display: flex;
+  }
+
+  .image-upload,
+  .form-inputs {
+    width: 50%;
+  }
+
+  label,
+  input {
+    display: block;
+  }
+
+  .star {
+    font-size: 20px;
+    color: var(--bg-secondary-color);
+    &:hover {
+      color: var(--text-secondary-color);
+    }
+  }
 `;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,22 +6,23 @@ const Home = () => {
   const { user, restaurants } = useGlobalContext();
 
   return (
-    <section>
-      <CardsContainer>
-        <h1 className="heading">{`Welcome ${user.name}!`}</h1>
-        <section className="cards">
-          {restaurants.map((res) => (
-            <Card key={res._id} restaurant={res} />
-          ))}
-        </section>
-      </CardsContainer>
-    </section>
+    // <section>
+    <CardsContainer>
+      <h1 className="heading">{`Welcome ${user.name}!`}</h1>
+      <section className="cards">
+        {restaurants.map((res) => (
+          <Card key={res._id} restaurant={res} />
+        ))}
+      </section>
+    </CardsContainer>
+    // </section>
   );
 };
 export default Home;
 
 const CardsContainer = styled.div`
   padding-right: var(--container-padding);
+  width: 100%;
 
   .cards {
     display: grid;
