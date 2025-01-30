a98f82f090cd769628eb68fc92f11710d904051a
SU-T <guitaristpeet@gmail.com>
2025-01-30 11:55:34 -0500

    highlighted stars when clicked

--- src/components/RateRangeEl.jsx
+++ src/components/RateRangeEl.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { TiStarFullOutline } from "react-icons/ti";
 
 const RateRangeEl = () => {
+  const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
   const arr = [1, 2, 3, 4, 5];
@@ -14,12 +15,13 @@ const RateRangeEl = () => {
           key={star}
           className="star"
           color={
-            star <= hover
+            star <= (hover || rating)
               ? "var(--text-secondary-color)"
               : "var(--bg-secondary-color)"
           }
           onMouseEnter={() => setHover(star)}
           onMouseLeave={() => setHover(0)}
+          onClick={() => setRating(star)}
         />
       ))}
     </RangeEl>
@@ -30,6 +32,7 @@ export default RateRangeEl;
 const RangeEl = styled.div`
   .star {
     font-size: 20px;
+    cursor: pointer;
     /* color: var(--bg-secondary-color); */
   }
 `;
