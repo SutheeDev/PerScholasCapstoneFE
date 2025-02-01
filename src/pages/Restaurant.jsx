cc1c4c2c4cfff5e849f83a74e41c5852f769d132
SU-T <guitaristpeet@gmail.com>
2025-02-01 16:48:44 -0500

    created DisplayRangeEl

--- src/pages/Restaurant.jsx
+++ src/pages/Restaurant.jsx
@@ -3,12 +3,16 @@ import { useGlobalContext } from "../App";
 import { Navbar } from "../components";
 import styled from "styled-components";
 import formatDate from "../utils/formatDate";
+import DisplayRangeEl from "../components/DisplayRangeEl";
 
+// Import Icons
 import { TiStarFullOutline } from "react-icons/ti";
 import { BiDollar } from "react-icons/bi";
 import { IoIosCloseCircleOutline } from "react-icons/io";
 import { FiEdit2 } from "react-icons/fi";
 import { GoKebabHorizontal } from "react-icons/go";
+import { FiCalendar } from "react-icons/fi";
+import { FiCoffee } from "react-icons/fi";
 
 const Restaurant = () => {
   const { id } = useParams();
@@ -21,39 +25,56 @@ const Restaurant = () => {
   const formattedDate = formatDate(date);
   console.log(formattedDate);
 
+  const rating = restaurant.rating;
+  const price = restaurant.priceRange.length;
+
   return (
     <main>
       <Navbar />
       <Content>
-        {/* <div className="page-wrapper"> */}
-        <div className="icons">
-          <IoIosCloseCircleOutline className="close-btn" />
-          <GoKebabHorizontal className="menu-btn" />
-        </div>
-        <div className="restaurant-content">
-          <div className="restaurant-img">
-            <img src={restaurant.image} alt={restaurant.name} />
+        <div className="page-wrapper">
+          <div className="icons">
+            <IoIosCloseCircleOutline className="close-btn" />
+            <GoKebabHorizontal className="menu-btn" />
           </div>
-          <div className="restaurant-details">
-            <h2>{restaurant.name}</h2>
-            <div>
-              <p>{formattedDate}</p>
-              <p>{restaurant.cuisine}</p>
+          <div className="restaurant-content">
+            <div className="restaurant-img">
+              <img src={restaurant.image} alt={restaurant.name} />
             </div>
-            <p>{restaurant.review}</p>
-            <div>
-              <div className="rating">
-                <p>Rating</p>
-                <TiStarFullOutline />
+            <div className="restaurant-details">
+              <h2>{restaurant.name}</h2>
+              <div className="date_cuisine">
+                <div className="date">
+                  <FiCalendar />
+                  <p>{formattedDate}</p>
+                </div>
+                <div className="cuisine">
+                  <FiCoffee />
+                  <p>{restaurant.cuisine}</p>
+                </div>
               </div>
-              <div className="price">
-                <p>Price</p>
-                <BiDollar />
+              <p className="review">{restaurant.review}</p>
+              <div>
+                <div className="rating">
+                  <p>Rating</p>
+                  <DisplayRangeEl
+                    Icon={TiStarFullOutline}
+                    numOfEl="5"
+                    highlightEl={rating}
+                  />
+                </div>
+                <div className="price">
+                  <p>Price</p>
+                  <DisplayRangeEl
+                    Icon={BiDollar}
+                    numOfEl="4"
+                    highlightEl={price}
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
-        {/* </div> */}
       </Content>
     </main>
   );
@@ -61,10 +82,8 @@ const Restaurant = () => {
 export default Restaurant;
 
 const Content = styled.div`
-  /* display: flex; */
   min-height: calc(100vh - 137px);
-  background-color: aliceblue;
-  /* position: relative; */
+  /* background-color: aliceblue; */
   padding: var(--container-padding);
 
   display: flex;
@@ -85,13 +104,6 @@ const Content = styled.div`
 
   .restaurant-img {
     width: 50%;
-    height: fit-content;
-    /* aspect-ratio: 1 / 1; */
-    /* background-color: var(--bg-secondary-color);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center; */
     border-radius: var(--card-radius);
     background-color: red;
     overflow: hidden;
@@ -99,25 +111,65 @@ const Content = styled.div`
 
   .restaurant-img img {
     width: 100%;
-    object-fit: cover;
-    /* display: none; */
+    display: block;
   }
 
   .restaurant-details {
     width: 50%;
   }
 
-  /* .close-btn,
-  .menu-btn {
-    position: absolute;
-    top: 0;
-    cursor: pointer;
-  }
   .close-btn {
-    left: var(--container-padding);
+    cursor: pointer;
+    width: var(--nav-icon-size);
+    height: var(--nav-icon-size);
+
+    transition: all 0.1s ease;
   }
 
   .menu-btn {
-    right: var(--container-padding);
-  } */
+    cursor: pointer;
+    width: var(--menu-icon-size);
+    height: var(--menu-icon-size);
+
+    transition: all 0.1s ease;
+  }
+
+  .close-btn:hover,
+  .menu-btn:hover {
+    transform: scale(1.05);
+  }
+
+  .restaurant-details h2 {
+    font-size: var(--header-size);
+  }
+
+  .date_cuisine,
+  .date,
+  .cuisine {
+    display: flex;
+    align-items: center;
+  }
+
+  .date_cuisine {
+    gap: 55px;
+  }
+
+  .date,
+  .cuisine {
+    gap: 12px;
+  }
+
+  .date p,
+  .cuisine p {
+    font-size: 20px;
+  }
+
+  .date svg,
+  .cuisine svg {
+    font-size: 24px;
+  }
+
+  .review {
+    max-width: 500px;
+  }
 `;
