5870998d5cf0ede0c9d9f7331c5a40e045509d29
SU-T <guitaristpeet@gmail.com>
2025-02-01 15:54:34 -0500

    basic style for restaurant page

--- src/pages/Restaurant.jsx
+++ src/pages/Restaurant.jsx
@@ -25,14 +25,19 @@ const Restaurant = () => {
     <main>
       <Navbar />
       <Content>
-        <div className="page-wrapper">
-          <IoIosCloseCircleOutline />
-          <GoKebabHorizontal />
-          <div className="restaurant-img"></div>
+        {/* <div className="page-wrapper"> */}
+        <div className="icons">
+          <IoIosCloseCircleOutline className="close-btn" />
+          <GoKebabHorizontal className="menu-btn" />
+        </div>
+        <div className="restaurant-content">
+          <div className="restaurant-img">
+            <img src={restaurant.image} alt={restaurant.name} />
+          </div>
           <div className="restaurant-details">
             <h2>{restaurant.name}</h2>
             <div>
-              <p>Date</p>
+              <p>{formattedDate}</p>
               <p>{restaurant.cuisine}</p>
             </div>
             <p>{restaurant.review}</p>
@@ -48,6 +53,7 @@ const Restaurant = () => {
             </div>
           </div>
         </div>
+        {/* </div> */}
       </Content>
     </main>
   );
@@ -57,4 +63,61 @@ export default Restaurant;
 const Content = styled.div`
   /* display: flex; */
   min-height: calc(100vh - 137px);
+  background-color: aliceblue;
+  /* position: relative; */
+  padding: var(--container-padding);
+
+  display: flex;
+  flex-direction: column;
+
+  .icons {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: var(--container-padding);
+  }
+
+  .restaurant-content {
+    display: flex;
+    align-items: flex-start;
+    gap: 55px;
+  }
+
+  .restaurant-img {
+    width: 50%;
+    height: fit-content;
+    /* aspect-ratio: 1 / 1; */
+    /* background-color: var(--bg-secondary-color);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center; */
+    border-radius: var(--card-radius);
+    background-color: red;
+    overflow: hidden;
+  }
+
+  .restaurant-img img {
+    width: 100%;
+    object-fit: cover;
+    /* display: none; */
+  }
+
+  .restaurant-details {
+    width: 50%;
+  }
+
+  /* .close-btn,
+  .menu-btn {
+    position: absolute;
+    top: 0;
+    cursor: pointer;
+  }
+  .close-btn {
+    left: var(--container-padding);
+  }
+
+  .menu-btn {
+    right: var(--container-padding);
+  } */
 `;
