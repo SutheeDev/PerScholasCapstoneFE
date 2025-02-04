0283ef026d50c03931531d5f77c83f5ff10c37c2
SU-T <guitaristpeet@gmail.com>
2025-02-04 11:55:59 -0500

    changed baseURL

--- src/utils/apiClient.jsx
+++ src/utils/apiClient.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://perscholascapstone.onrender.com/api",
   headers: { "Content-Type": "application/json" },
 });
